42cc7ca2abcebdbb99c3837124bb4b317110ac1b
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-27 14:38:45 +0530

    created express server

---
+++ app.js
@@ -0,0 +1,7 @@
+// Importing the express library
+const express = require("express");
+
+// Creating an express application
+const app = express();
+
+module.exports = app;
--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 const mongoose = require("mongoose");
 
 // Importing values from config
-const { MONGODB_URI } = require("./utils/config");
+const { MONGODB_URI, PORT } = require("./utils/config");
+
+// Importing the express app
+const app = require("./app");
 
 // Connecting to mongoDB
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((error) => {
     console.log("Error connecting to MongoDB:", error.message);
