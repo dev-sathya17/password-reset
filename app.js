8e4f63dd419b1b10503eed484901dc645caf80a3
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-27 18:07:29 +0530

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // Importing the express library
 const express = require("express");
+
+// Importing cors
+const cors = require("cors");
+
+// Importing routes
 const userRouter = require("./routes/userRoutes");
 
 // Creating an express application
 const app = express();
+
+// adding cors middleware for API calls from react
+app.use(cors());
 
 // Adding a middleware to parse request body as json
 app.use(express.json());
