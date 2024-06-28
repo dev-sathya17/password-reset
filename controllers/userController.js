13fb6768c1a28ce9e05a289761e5f8d0c09245d0
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-28 13:13:19 +0530

    added status code

--- controllers/userController.js
+++ controllers/userController.js
@@ -91,7 +91,7 @@ const userController = {
       // Checking if this auth string is of a valid user
       const user = await User.findOne({ authString });
       if (!user) {
-        return res.json({ message: "Auth string does not match!" });
+        return res.status(404).json({ message: "Auth string does not match!" });
       }
 
       // Sending a success response
